c2e913d4af152d214fd0ec49c729e5c901603435
craignelstead <ctnelst92@gmail.com>
2023-09-29 12:30:20 -0600

    Clicking on square shows object info in console.

--- script.js
+++ script.js
@@ -1,67 +1,87 @@
 //Game board module
 const GameBoard = (function(doc) {
+    //Array of divs making up the TTT grid
     const squares = Array.from(doc.querySelectorAll('.tttsquare'));
 
-    //Factory function for each square
+    //Creates an object for each square using factory function
     //Contains Row, Column, and current selection (null by default)
     const squareFactory = (currentSquare) => {
         const gridRow = currentSquare.getAttribute('data-row');
         const gridCol = currentSquare.getAttribute('data-col');
-        let gridSelection = null;
+        let gridSelection = currentSquare.textContent;
 
-        const showStatus = (currentSquare) => {
-            currentSquare.addEventListener('click', function(event) {
-                let row = event.target.getAttribute('data-row');
-                let col = event.target.getAttribute('data-col')
-                console.log(`Row ${row} Col ${col}`);
-            })
+        //Method to see current selection of square
+        const checkSquareStatus = () => {
+            console.log(gridSelection);
         }
+
+        //Add event listener to each square
+        //Event listener will check the current status of each square
+        currentSquare.addEventListener('click', function() {
+            console.log(`Row ${gridRow} Col ${gridCol}`);
+            checkSquareStatus(currentSquare);
+
+            currentSquare.textContent = gamePlay.currentTurn;
+            gridSelection = currentSquare.textContent;
+            //gridSquare[currentSquare].gridSelection = gamePlay.currentTurn;
+        });
 
         return {
             gridRow,
             gridCol,
             gridSelection,
-            showStatus,
         }
     }
 
-    //Create an object for each square
+    //Create an object for each square using squareFactory and push objects
+    //into gridSqaure array
     const gridSquare = [];
     for (let i = 0; i < squares.length; i++) {
         gridSquare.push(squareFactory(squares[i]));
     }
-
-    //Add event listener to each square
-    // for (let i = 0; i < squares.length; i++) {
-    //     squares[i].addEventListener('click', function(event) {
-    //         let row = event.target.getAttribute('data-row');
-    //         let col = event.target.getAttribute('data-col')
-    //         console.log(`Row ${row} Col ${col}`);
-    //     });
-    // }
 
     return {
         gridSquare,
     };
 })(document);
 
-//Game play module
-const GamePlay = (function() {
-
-    //Method to see if square is already taken
-    const checkSquareStatus = () => {
-
-    }
-    
-    //Display Controller
-
-});
-
 //Factory function to create each player
 const Player = (name) => {
     const getName = () => name;
+    let score = 0;
 
     return {
         getName,
     }
 }
+
+//Game play module
+const gamePlay = (function() {
+
+    //Create players
+    const playerX = Player('X');
+    const playerO = Player('O');
+
+    //Default so player X goes first
+    let currentTurn = 'X';
+
+    //Change turn to other player
+    const changeTurn = () => {
+        if (currentTurn === 'X') {currentTurn = 'O'}
+        else {currentTurn = 'X'}
+    }
+    
+    //Check if valid placement
+    const testPlacement = () => {
+        if (this.gridSelection === '') {
+            console.log(this.gridSelection);
+        }
+    }
+
+    return {
+        playerX,
+        playerO,
+        currentTurn,
+    }
+})();
+
